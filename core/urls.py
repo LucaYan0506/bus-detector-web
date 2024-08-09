1bc38d8138c24ddfb697e5f4a179c0222840ba51
LucaYan0506 <lucayan0506@gmail.com>
2024-08-09 10:51:49 +0100

    add new feature:detect youtube vidoe

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.indexView, name="index"),
     path('detect/', views.detectView, name="detect"),
+    path('get_video',views.getVideoView, name="get_video")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse, HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 from .predict_video import process_video
 from django.conf import settings
-import os 
+import os
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 
 # Create your views here.
 
@@ -23,3 +25,18 @@ def detectView(request):
         else:
             return JsonResponse({'error': 'Invalid file type'}, status=400)
     return JsonResponse({'error': 'No video file provided'}, status=400)
+
+def getVideoView(request):
+    if request.method == 'POST' and 'link' in request.POST:
+        try:
+            yt = YouTube(request.POST['link'], on_progress_callback = on_progress)
+            yt.check_availability()
+        except:
+            return JsonResponse({'error': 'Invalid url'}, status=400)
+        
+        stream = yt.streams.filter(file_extension='mp4').first()
+        stream.download(output_path=settings.MEDIA_ROOT,filename="youtubeVideo.mp4")
+        model_path = os.path.join(settings.MEDIA_ROOT,'last.pt')
+        process_video(os.path.join(settings .MEDIA_ROOT,'youtubeVideo.mp4'),model_path)
+        return HttpResponseRedirect('/media/result.mp4')
+    return JsonResponse({'error': 'No url provided'}, status=400)
